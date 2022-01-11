d76173ca92c9bce3ed7c4e1c5c1879032e190bc2
Douglas Andrade <dougandra1@gmail.com>
2022-01-11 15:41:05 -0300

    jogo resilia

---
+++ fase1.js
@@ -0,0 +1,15 @@
+
+function lana(){
+    alert("LANA: enigmática, corajosa, dinânima, gosta de desafios e adora desvendar mistérios. Certamente a mais destemida das três. Dificilmente recua quando o assunto é aventuras e se diverte muito ao enfrentar medos.");
+    location.assign("./game5.html");
+}
+
+function mary(){
+    alert("MARY: a mais sensível e medrosa da turma. Nunca aceita desafios de cara e pensa muito ao aceitar aventuras com as irmãs. Pode-se dizer que ela seja a mais tranquila das três, com uma observação impecável.");
+    location.assign("./game5.html");
+}
+
+function lauren(){
+    alert("LAUREN: a sensitiva e bruxa. Adora participar de rituais com magias da natureza. Conhece como ninguém a arte de perceber ambientes e detecta de longe presenças sobrenaturais.");
+    location.assign("./game5.html");
+}
---
+++ fase2.js
@@ -0,0 +1,98 @@
+
+function load(){
+
+    let option = parseInt(prompt("Deseja seguir em frente?" + "\n" + "Digite o número da opção abaixo:" + "\n" +
+    "\n" + 1 + "-" + "Sim" +
+    "\n" + 2 + "-" + "Não" +
+    "\n"));  
+
+
+     if( option == 1){
+
+        location.assign("./game6.html");
+     }
+
+
+ }
+
+
+ function load2(){
+
+    let option2 = parseInt(prompt("Deseja seguir em frente?" + "\n" + "Digite o número da opção abaixo:" + "\n" +
+              "\n" + 1 + "-" + "Sim" +
+              "\n" + 2 + "-" + "Não" +
+              "\n")); 
+    
+    
+         if( option2 == 1){
+    
+            location.assign("./game7.html");
+         }
+         
+         else if( option2 == 2){
+
+            location.assign("./game5.html");
+         }
+         
+         else{
+
+           alert("Por favor, digite 1 ou 2:");
+         }
+        }
+
+
+
+        function load3(){
+
+            let option3 = parseInt(prompt("Deseja seguir em frente?" + "\n" + "Digite o número da opção abaixo:" + "\n" +
+                      "\n" + 1 + "-" + "Sim" +
+                      "\n" + 2 + "-" + "Não" +
+                      "\n")); 
+            
+            
+                 if( option3 == 1){
+            
+                    location.assign("./game7copy.html");
+                 }
+                 
+                 else if( option3 == 2){
+        
+                    location.assign("./game5.html");
+                 }
+                 
+                 else{
+        
+                   alert("Por favor, digite 1 ou 2:");
+                 }
+                }
+
+
+
+
+                function load4(){
+
+                    let option2 = parseInt(prompt("Deseja seguir em frente?" + "\n" + "Digite o número da opção abaixo:" + "\n" +
+                              "\n" + 1 + "-" + "Sim" +
+                              "\n" + 2 + "-" + "Não" +
+                              "\n")); 
+                    
+                    
+                         if( option2 == 1){
+                           
+                            location.assign("./game10.html");
+                         }
+                         
+                         else if( option2 == 2){
+                
+                            location.assign("./game5.html");
+                         }
+                         
+                         else{
+                
+                           alert("Por favor, digite 1 ou 2:");
+                         }
+                        }
+        
+                       
+                       
+                
